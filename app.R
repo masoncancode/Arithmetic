77b26b2310ca4122c682a329c8f6dcecc6a95db5
Mason Reed <twizzy@wireless-10-146-1-249.public.utexas.edu>
2025-09-30 10:15:19 -0500

    commit

--- app.R
+++ app.R
@@ -9,7 +9,9 @@ ui <- fluidPage(
                          value = 0),
             numericInput(inputId = "number2",
                          label = "Number 2",
-                         value = 0)
+                         value = 0),
+            actionButton(inputId = "click",
+                         label = "add :)")
         ),
         mainPanel(
             h2("The sum of the two numbers is:"),
@@ -19,10 +21,15 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
+    x1 <- eventReactive(input$click, {
+      as.numeric(input$number1)
+    })
+    x2 <- eventReactive(input$click, {
+      as.numeric(input$number2)
+    })
     output$sum <- renderText({
-        x1 <- as.numeric(input$number1)
-        x2 <- as.numeric(input$number2)
-        x1 + x2
+        
+        x1() + x2()
     })
 }
 
